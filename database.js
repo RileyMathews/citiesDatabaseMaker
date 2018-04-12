220c86545226ecc41051893c6b1456acc5f03172
Riley Mathews <rileymathews80@gmail.com>
2018-04-12 12:44:37 -0500

    initial commit

---
+++ database.js
@@ -0,0 +1,78 @@
+const database = {}
+
+database.cities = []
+
+// citties will be: venice 2008, paris 2011, rome 2008, new york 2007, Port Lockroy 2010, san antonio 2011
+
+database.cities.push(
+    {
+        name: "Singapore",
+        visited: "2018",
+        continent: "asia",
+        image: "https://www.100resilientcities.org/wp-content/uploads/2017/06/Singapore-hero-cro.jpg",
+        attractions: ["Marina Bay Observation Deck", "Garden Conservatory", "Botanical Gardens", "River Cruise", "Historic Tour"]
+    },
+    {
+        name: "London",
+        visited: "2013",
+        continent: "europe",
+        image: "https://www.pexels.com/search/london/",
+        attractions: ["Buckingham Palace", "Hard Rock Cafe Vault", "Abbey Road Studio", "London Eye", "Royal Philharmonic Orchestra"]
+    },
+    {
+        name: "Dublin",
+        visited: "2012",
+        continent: "europe",
+        image: "https://d5qsyj6vaeh11.cloudfront.net/images/destinations/dublin/dublin-main/ten-attractions/dublin-top-ten-hapenny-bridge-bg.jpg",
+        attractions: ["Pub crawls", "Irish music jams", "Blarney Castle", "Trinity College Exhibits and Displays", "Guinness Storehouse"]
+    },
+    {
+        name: "Edinburgh",
+        visited: "2012",
+        continent: "europe",
+        image: "https://www.edinburghchamber.co.uk/",
+        attractions: ["Edinburgh Castle", "Holyrood Park", "The Elephant House", "Grayfriers Kirk", "North Bridge"]
+    },
+    {
+        name: "Dublin",
+        visited: "2012",
+        continent: "europe",
+        image: "",
+        attractions: []
+    },
+    {
+        name: "Dublin",
+        visited: "2012",
+        continent: "europe",
+        image: "",
+        attractions: []
+    },
+    {
+        name: "Dublin",
+        visited: "2012",
+        continent: "europe",
+        image: "",
+        attractions: []
+    },
+    {
+        name: "Dublin",
+        visited: "2012",
+        continent: "europe",
+        image: "",
+        attractions: []
+    },
+    {
+        name: "Dublin",
+        visited: "2012",
+        continent: "europe",
+        image: "",
+        attractions: []
+    },
+    {
+        name: "Dublin",
+        visited: "2012",
+        continent: "europe",
+        image: "",
+        attractions: []
+    },
+)
